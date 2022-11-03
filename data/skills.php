5e907b6933dd0a2ccca3972ce92aefb674482aba
Pedro Cesar Costa <pedro.megadeth@hotmail.com>
2022-11-03 09:58:50 -0300

    Update skills.php

--- data/skills.php
+++ data/skills.php
@@ -6,56 +6,49 @@ $skills = [
         'id' => 1,
         'img' => 'html5.png',
         'titulo' => 'HTML 5',
-        'descricao' => 'HTML 5 Lorem Ipsum is simply dummy text of the 
-        printing and typesetting industry.',
+        'descricao' => 'HTML5 é uma linguagem de marcação para a World Wide Web e é uma tecnologia chave da Internet.',
     ],
 
     [
         'id' => 2,
         'img' => 'css3.png',
         'titulo' => 'CSS 3',
-        'descricao' => 'CSS 3 Lorem Ipsum is simply dummy text of the 
-        printing and typesetting industry.',
+        'descricao' => 'CSS3 é a terceira mais nova versão das famosas Cascading Style Sheets, pela qual se define estilos para um projeto web.',
     ],
 
     [
         'id' => 3,
         'img' => 'javascript.png',
         'titulo' => 'JavaScript',
-        'descricao' => 'JavaScript Lorem Ipsum is simply dummy text of the 
-        printing and typesetting industry.',
+        'descricao' => 'JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma.',
     ],
 
     [
         'id' => 4,
         'img' => 'php.png',
         'titulo' => 'PHP',
-        'descricao' => 'PHP Lorem Ipsum is simply dummy text of the 
-        printing and typesetting industry.',
+        'descricao' => 'PHP é uma linguagem interpretada livre, usada originalmente apenas para o desenvolvimento de aplicações presentes e atuantes no lado do servidor.',
     ],
 
     [
         'id' => 5,
         'img' => 'mysql.png',
         'titulo' => 'MySQL',
-        'descricao' => 'MySQL Lorem Ipsum is simply dummy text of the 
-        printing and typesetting industry.',
+        'descricao' => 'O MySQL é um sistema de gerenciamento de banco de dados, que utiliza a linguagem SQL como interface.',
     ],
 
     [
         'id' => 6,
         'img' => 'jquery.png',
         'titulo' => 'JQuery',
-        'descricao' => 'JQuery Lorem Ipsum is simply dummy text of the 
-        printing and typesetting industry.',
+        'descricao' => 'jQuery é uma biblioteca livre que contém funções da linguagem de programação JavaScript que interage com páginas em HTML.',
     ],
 
     [
         'id' => 7,
         'img' => 'bootstrap.png',
         'titulo' => 'Bootstrap',
-        'descricao' => 'Bootstrap Lorem Ipsum is simply dummy text of the 
-        printing and typesetting industry.',
+        'descricao' => 'Bootstrap é um framework web com código-fonte aberto para desenvolvimento de componentes de interface e front-end para sites e aplicações web, usando HTML.',
     ],
 
 ];
